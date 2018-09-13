b43cd6b2768e06ec9ad04f7d02b18949e90623e9
Nicholas Wijaya <pinkappleberry98@gmail.com>
2018-09-13 16:58:02 +0700

    add attr color

--- objects/piece.py
+++ objects/piece.py
@@ -1,7 +1,8 @@
 class Piece:
-    def __init__(self, x, y):
+    def __init__(self, x, y, color):
         self.x = x
         self.y = y
+        self.color = color
 
     def attack(self, board):
         pass
