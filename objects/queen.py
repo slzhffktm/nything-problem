ebdbcb17ed7150f5354be94919f2caae3801a2a3
Nicholas Wijaya <pinkappleberry98@gmail.com>
2018-09-13 17:16:32 +0700

    create queen class

---
+++ objects/queen.py
@@ -0,0 +1,94 @@
+from piece import Piece
+
+class Queen(Piece):
+
+    # constructor
+    def __init__(self, x, y, color):        
+        Piece.__init__(self, x, y, color)
+
+
+    # return how many conflicts occured
+    def attack(self, board):
+        conflicts = 0
+
+        # diagonal
+        # left-down
+        j = self.y
+        for i in range(self.x-1, -1, -1):
+            if j - 1 < 0:
+                break
+            else:
+                j = j - 1
+                if board.check(i, j):
+                    conflicts += 1
+                    break
+        
+        # left-up
+        j = self.y
+        for i in range(self.x-1, -1, -1):
+            if j + 1 < 0:
+                break
+            else:
+                j = j + 1
+                if board.check(i, j):
+                    conflicts += 1
+                    break
+
+        # right-up
+        j = self.y
+        for i in range(self.x+1, 8):
+            if j + 1 < 0:
+                break
+            else:
+                j = j + 1
+                if board.check(i, j):
+                    conflicts += 1
+                    break
+
+        # right-down
+        j = self.y
+        for i in range(self.x+1, 8):
+            if j - 1 < 0:
+                break
+            else:
+                j = j - 1
+                if board.check(i, j):
+                    conflicts += 1
+                    break
+
+        # vertical and horizontal
+        north = True
+        south = True
+        west = True
+        east = True
+        for i in range (1,7):
+            if (north and (self.y + i <= 7)):
+                if (board.check(self.x, self.y + i)):
+                    conflicts += 1
+                    north = False
+            else:
+                north = False
+
+            if (south and (self.y - i >= 0)):
+                if (board.check(self.x, self.y - i)):
+                    conflicts += 1
+                    south = False
+            else:
+                south = False
+
+            if (west and (self.x + i <= 7)):
+                if (board.check(self.x + i, self.y)):
+                    conflicts += 1
+                    west = False
+            else:
+                west = False
+            
+            if (east and (self.x - i >= 0)):
+                if (board.check(self.x - i, self.y)):
+                    conflicts += 1
+                    east = False
+            else:
+                east = False
+
+        return conflicts
+
--- objects/test_attack.py
+++ objects/test_attack.py
@@ -1,13 +1,13 @@
-from rook import Rook
+from queen import Queen
 
 class Board:
     
     def check(self, i, j):
-        if (i == 5 and j == 3) or (i == 5 and j == 4):
+        if (i == 6 and j == 6) or (i == 5 and j == 3) or (i == 7 and j == 7):
             return True
         return False
 
 board = Board()
-bishop = Rook(5, 5)
+bishop = Queen(5, 5, "red")
 
 print(bishop.attack(board))
