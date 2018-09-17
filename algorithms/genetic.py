eb3da22f0333d1ab89ed7325e70a503672033ea5
ayrtoncn <ayrton.niwarlangga@gmail.com>
2018-09-18 06:30:09 +0700

    crossover and many more

--- algorithms/genetic.py
+++ algorithms/genetic.py
@@ -6,15 +6,15 @@ global queenPossibleMoves = 8
 global bishopPossibleMoves = 4
 global rookPossibleMoves = 4
 
+def maxFitnessFunction(board):
+    return maxFitnessFunction = (board.whiteKnight + board.blackKnight) * knightPossibleMoves +
+                                    (board.whiteQueen + board.blackQueen) * queenPossibleMoves +
+                                    (board.whiteBishop + board.blackBishop) * bishopPossibleMoves +
+                                    (board.whiteRook + board.blackRook) * rookPossibleMoves
 
-
-def countFitnessFunction(board):
-    maxFitnessFunction = (board.whiteKnight + board.blackKnight) * knightPossibleMoves +
-                            (board.whiteQueen + board.blackQueen) * queenPossibleMoves +
-                            (board.whiteBishop + board.blackBishop) * bishopPossibleMoves +
-                            (board.whiteRook + board.blackRook) * rookPossibleMoves
-    conflict = board.countConflictsSameColor()
-    return maxFitnessFunction - conflict
+def countFitnessFunction(board, pieces):
+    conflict = board.countConflictsSameColor(pieces)
+    return maxFitnessFunction(board) - conflict
 
 def selection(pieces, side, idxSelection):
     selectionPieces = []
@@ -40,9 +40,36 @@ def mutation(pieces, maps):
     idxSelection = rnd.randint(0, len(pieces1))
     newX = rnd.randint(0,7)
     newY = rnd.randint(0,7)
-    while(self.maps[7 - newY][newX] != '.'):
+    while(maps[7 - newY][newX] != '.'):
         newX = rnd.randint(0,7)
         newY = rnd.randint(0,7)
     newY = 7 - newY
     pieces[idxSelection].x = newX
     pieces[idxSelection].y = newY
+    return pieces
+
+def randAllPiece(pieces, maps):
+    for piece in pieces:
+        newX = rnd.randint(0,7)
+        newY = rnd.randint(0,7)
+        while(maps[7 - newY][newX] != '.'):
+            newX = rnd.randint(0,7)
+            newY = rnd.randint(0,7)
+        newY = 7 - newY
+        piece.x = newX
+        piece.y = newY
+    return pieces
+
+def genetic(board):
+    nPopulation = 4
+    population = []
+    for i in range (nPopulation):
+        temp = randAllPiece(board.pieces, board.maps)
+        population.append([temp, countFitnessFunction(board, temp)])
+    cleanPopulation = []
+    for individual in population:
+        if (individual[1] > maxFitnessFunction(board)/4):
+            cleanPopulation.append(individual)
+    cleanPopulation.sort(key=lambda x:x[1])
+    for 
+    
