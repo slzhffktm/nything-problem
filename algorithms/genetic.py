7b213f6b8c379c1a13c2d09acead06fe510d8cf0
ericjonathan6 <eric.jonathann15@gmail.com>
2018-09-18 16:41:42 +0700

    can already find solution

--- algorithms/genetic.py
+++ algorithms/genetic.py
@@ -1,16 +1,14 @@
 import random as rnd
+import copy as cpy
 from objects.board import Board
 
-global knightPossibleMoves = 8
-global queenPossibleMoves = 8
-global bishopPossibleMoves = 4
-global rookPossibleMoves = 4
 
 def maxFitnessFunction(board):
-    return maxFitnessFunction = (board.whiteKnight + board.blackKnight) * knightPossibleMoves +
-                                    (board.whiteQueen + board.blackQueen) * queenPossibleMoves +
-                                    (board.whiteBishop + board.blackBishop) * bishopPossibleMoves +
-                                    (board.whiteRook + board.blackRook) * rookPossibleMoves
+    knightPossibleMoves = 8
+    queenPossibleMoves = 8
+    bishopPossibleMoves = 4
+    rookPossibleMoves = 4
+    return (board.whiteKnight + board.blackKnight) * knightPossibleMoves + (board.whiteQueen + board.blackQueen) * queenPossibleMoves + (board.whiteBishop + board.blackBishop) * bishopPossibleMoves + (board.whiteRook + board.blackRook) * rookPossibleMoves
 
 def countFitnessFunction(board, pieces):
     conflict = board.countConflictsSameColor(pieces)
@@ -24,7 +22,7 @@ def selection(pieces, side, idxSelection):
     elif(side == 'right'):
         for i in range(idxSelection, len(pieces)):
             selectionPieces.append(pieces[i])
-    return selectionBoard
+    return selectionPieces
             
 
 def crossOver(pieces1,pieces2):
@@ -32,12 +30,19 @@ def crossOver(pieces1,pieces2):
     part1 = selection(pieces1, 'left', idxSelection)
     part2 = selection(pieces2, 'right', idxSelection)
     result = []
-    result.append(part1)
-    result.append(part2)
+    for leftSide in part1:
+        result.append(leftSide)
+    for rightSide in part2:
+        result.append(rightSide)
+    for leftSide in part1:
+        for rightSide in part2:
+            if (leftSide.x == rightSide.x and leftSide.y == rightSide.y):
+                return []
+    # print(result)
     return result
 
 def mutation(pieces, maps):
-    idxSelection = rnd.randint(0, len(pieces1))
+    idxSelection = rnd.randint(0, len(pieces))
     newX = rnd.randint(0,7)
     newY = rnd.randint(0,7)
     while(maps[7 - newY][newX] != '.'):
@@ -49,27 +54,61 @@ def mutation(pieces, maps):
     return pieces
 
 def randAllPiece(pieces, maps):
+    tempMaps = cpy.deepcopy(maps)
     for piece in pieces:
         newX = rnd.randint(0,7)
         newY = rnd.randint(0,7)
-        while(maps[7 - newY][newX] != '.'):
+        while(tempMaps[7 - newY][newX] != '.'):
             newX = rnd.randint(0,7)
             newY = rnd.randint(0,7)
         newY = 7 - newY
         piece.x = newX
         piece.y = newY
+        tempMaps[piece.y][piece.x] = piece.getChar()
     return pieces
 
 def genetic(board):
-    nPopulation = 4
+    nPopulation = 100
     population = []
+    # print("temp")
     for i in range (nPopulation):
-        temp = randAllPiece(board.pieces, board.maps)
+        temp = cpy.deepcopy(randAllPiece(board.pieces, board.maps))
         population.append([temp, countFitnessFunction(board, temp)])
     cleanPopulation = []
+    population.sort(key=lambda x:x[1])
+    # for i in range(len(population)):
+    #     print("count")
+    #     print(population[i][1])
     for individual in population:
-        if (individual[1] > maxFitnessFunction(board)/4):
+        if (individual[1] > maxFitnessFunction(board)/2):
             cleanPopulation.append(individual)
-    cleanPopulation.sort(key=lambda x:x[1])
-    for 
-    
+    # print(cleanPopulation)
+    cleanPopulation.sort(key=lambda x:x[1], reverse=True)
+    # print("test")
+    # print(cleanPopulation)
+    nCleanPopulation = len(cleanPopulation)
+    # child = [cleanPopulation[0][0]]
+    for j in range(1000):
+        if(cleanPopulation[0][1] != maxFitnessFunction(board)):
+            # print("clean")
+            for i in range(len(cleanPopulation)-1):
+                if (i+1 < len(cleanPopulation)):
+                    child = crossOver(cleanPopulation[i][0],cleanPopulation[i+1][0])
+                    # print("anak",i)
+                # else:
+                #     child = crossOver(cleanPopulation[0][0],cleanPopulation[i][0])
+                if (child != []):
+                    board.update(child)
+                    # board.show()
+                    cleanPopulation.append([child, countFitnessFunction(board, child)])
+            cleanPopulation.sort(key=lambda x:x[1], reverse=True)
+            cleanPopulation = cleanPopulation[:nCleanPopulation]
+            # cleanPopulation = cleanPopulation[:nCleanPopulation]
+            # print("LIST POPULATION")
+            # print(cleanPopulation)
+        else:
+            break
+    print("Genetic Algorithm")
+    board.update(cleanPopulation[0][0])
+    print("Solution")
+    board.show()    
