fix hillclimbing calculation

algorithms/hillClimbing.py
@@ -3,14 +3,15 @@ import random as rnd
 import numpy as np
 
 def countHeatMap(board,piece, pieceChar,tempPieces):
-        mapsMin =  np.zeros((8,8), dtype=np.int)
-        mapsMax =  np.zeros((8,8), dtype=np.int)
+        mapsDiiference =  np.zeros((8,8), dtype=np.int)
+        # mapsMax =  np.zeros((8,8), dtype=np.int)
         for i in range(8):
             for j in range(8):
                 if board.maps[i][j] != ".":
                     # dummy for imposible move
-                    mapsMin[i][j] = 999
-                    mapsMax[i][j] = -999
+                    # mapsMin[i][j] = 999
+                    # mapsMax[i][j] = -999
+                    mapsDiiference[i][j] = 999
                 else:
                     # insert pieceChart(Q,B,R,K) to maps
                     board.maps[i][j] = pieceChar
@@ -18,11 +19,12 @@ def countHeatMap(board,piece, pieceChar,tempPieces):
                     piece.y = i
                     # insert new piece with new i and j to tempPieces
                     tempPieces.append(piece)
-                    mapsMin[i][j], mapsMax[i][j] = board.countConflicts(tempPieces)
+                    minValue, maxValue = board.countConflicts(tempPieces)
+                    mapsDiiference[i][j] = minValue - maxValue
                     # remove again the new piece
                     tempPieces.remove(piece)
                     board.maps[i][j] = '.'
-        return mapsMin, mapsMax
+        return mapsDiiference
 
 
 def hillClimbing(board):
@@ -42,37 +44,34 @@ def hillClimbing(board):
                 board.maps[piece.y][piece.x] = '.'
                 tempPiece = copy.deepcopy(piece)
                 # create heat map for minimum and maximum
-                minHeatMap, maxHeatMap = countHeatMap(board, tempPiece, pieceChar, tempPieces)
+                # minHeatMap, maxHeatMap = countHeatMap(board, tempPiece, pieceChar, tempPieces)
+                differenceHeatMap = countHeatMap(board, tempPiece, pieceChar, tempPieces)
                 # get minimum value and maximum
-                minValue = minHeatMap.min()
-                maxValue = maxHeatMap.max()
+                # minValue = minHeatMap.min()
+                # maxValue = maxHeatMap.max()
+                minValue = differenceHeatMap.min()
                 # compare minValue and maxValue and choose the more benefit
-                if((sameColor - minValue) > (difColor - maxValue)):
+                if((minValue) < (sameColor - difColor)):
                     moveToMin = True
-                    moveToMax = False
-                    maxValue = -999
                 else:
-                    moveToMax = True
                     moveToMin = False
-                    minValue = 999
                 minIdx = []
-                maxIdx = []
+                # maxIdx = []
                 # find minimum index or maximum index
                 for i in range(8):
                     for j in range(8):
-                        if(moveToMin and minHeatMap[i][j] == minValue):
+                        if(moveToMin and differenceHeatMap[i][j] == minValue):
                             minIdx.append((j,i))
-                        if(moveToMax and maxHeatMap[i][j] == maxValue):
-                            maxIdx.append((j,i))
-                if not(minValue == sameColor or maxValue == difColor):
+                            
+                if not(minValue == sameColor):
                     # choice index from list of minimum index
                     # append minimum minimum value and index to be compared with other pieces
                     if(moveToMin):
                         newIdx = rnd.choice(minIdx)
-                        piecesMovement.append([sameColor - minValue,newIdx,piece])
-                    else:
-                        newIdx = rnd.choice(maxIdx)
-                        piecesMovement.append([difColor - maxValue,newIdx,piece])                  
+                        piecesMovement.append([minValue,newIdx,piece])
+                    # else:
+                    #     newIdx = rnd.choice(maxIdx)
+                    #     piecesMovement.append([maxValue - difColor,newIdx,piece])                  
                     isPieceMove = True
                 board.maps[piece.y][piece.x] = pieceChar
                 tempPieces = board.pieces[:]